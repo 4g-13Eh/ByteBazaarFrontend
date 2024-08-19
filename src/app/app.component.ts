4a9b1f45b8dee38a3c30cc8f4ec09dcc13a2daf7
4g-13Eh <shahinamon05@gmail.com>
2024-08-19 15:34:09 +0200

    refactor structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ItemComponent} from "./item/item.component";
 import {HeaderComponent} from "./header/header.component";
+import {ContentCardComponent} from "./ui/content-card/content-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ItemComponent, HeaderComponent],
+  imports: [RouterOutlet, ItemComponent, HeaderComponent, ContentCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
