de3a802626e5f089ebcd46ba37886a84c73e46c8
4g-13Eh <shahinamon05@gmail.com>
2024-08-19 14:37:45 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ItemComponent} from "./item/item.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ItemComponent],
+  imports: [RouterOutlet, ItemComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ByteBazaar';
+  searchQuery: string = '';
 }
