dac4e45f23cb314b30083811536a05549f5f5a39
4g-13Eh <shahinamon05@gmail.com>
2024-08-21 10:02:35 +0200

    update routes

--- src/app/routes/items.routes.ts
+++ src/app/routes/items.routes.ts
@@ -4,11 +4,6 @@ import {ItemComponent} from "../items/item/item.component";
 
 export const itemRoutes: Routes = [
   {
-    path: '',
-    redirectTo: 'items',
-    pathMatch: 'full',
-  },
-  {
     path: 'items',
     component: ItemsComponent,
   },
