3c1b61d096c4f17d65876a8b009a7b2a416f2c71
4g-13Eh <shahinamon05@gmail.com>
2024-08-28 12:35:03 +0200

    add payment comp

---
+++ src/app/ui/paymethod/paymethod.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatFormField} from "@angular/material/form-field";
+
+@Component({
+  selector: 'app-paymethod',
+  standalone: true,
+  imports: [
+    MatFormField
+  ],
+  templateUrl: './paymethod.component.html',
+  styleUrl: './paymethod.component.css'
+})
+export class PaymethodComponent {
+
+}
