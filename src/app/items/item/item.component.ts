e700876de3be75a240d6619b83d8e0a863ecfecf
4g-13Eh <shahinamon05@gmail.com>
2024-08-28 11:36:37 +0200

    code optimized

--- src/app/items/item/item.component.ts
+++ src/app/items/item/item.component.ts
@@ -43,8 +43,6 @@ export class ItemComponent implements OnInit, OnDestroy{
   addToCart(){
     this.cartService.addItemToCart({item: this.item, quantity: 1});
     console.log(this.cartService.getCartItems());
-    // console.log('added to cart');
-    // console.log(this.cartService.getCartItems());
   }
 
 }
