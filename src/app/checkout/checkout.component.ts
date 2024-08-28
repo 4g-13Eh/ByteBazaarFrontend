cf46e00fd5125a6dd5b301dd3f8281ff8609a061
4g-13Eh <shahinamon05@gmail.com>
2024-08-28 12:36:49 +0200

    add checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ShoppingCartService} from "../shopping-cart/shopping-cart.service";
+import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
+import {PaymethodComponent} from "../ui/paymethod/paymethod.component";
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    PaymethodComponent
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  private cartService = inject(ShoppingCartService);
+  cartItems = this.cartService.getCartItems();
+  private router = inject(Router);
 
+  buyItems(){
+    return this.cartService
+  }
+
+  backToCart(){
+    this.router.navigate(['/cart']);
+  }
+
+  protected readonly Math = Math;
 }
