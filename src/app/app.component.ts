c8119b02906625d40a9bec11193998dc3cd90038
4g-13Eh <shahinamon05@gmail.com>
2024-08-29 11:16:37 +0200

    removed unnecessary ui-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import {ItemsComponent} from "./items/items.component";
-import {ContentCardComponent} from "./ui/content-card/content-card.component";
 import {TooltipComponent} from "./ui/tooltip/tooltip.component";
 import {HeaderComponent} from "./header/header.component";
 import {SearchfieldComponent} from "./ui/searchfield/searchfield.component";
@@ -9,7 +8,7 @@ import {SearchfieldComponent} from "./ui/searchfield/searchfield.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ItemsComponent, ContentCardComponent, TooltipComponent, HeaderComponent, SearchfieldComponent],
+  imports: [RouterOutlet, ItemsComponent, TooltipComponent, HeaderComponent, SearchfieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
