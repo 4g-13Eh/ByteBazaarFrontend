babfa0371aa11f632702754496a7593d59970e0c
4g-13Eh <shahinamon05@gmail.com>
2024-08-29 11:53:24 +0200

    cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from "./auth/login/login.component";
-import {ItemsComponent} from "./items/items.component";
 import {itemRoutes} from "./routes/items.routes";
 import {authRoutes} from "./routes/auth.routes";
-import {SignupComponent} from "./auth/signup/signup.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 import {CheckoutComponent} from "./checkout/checkout.component";
 
