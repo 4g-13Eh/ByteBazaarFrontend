edc359e44bfb774128949344ab0728b07f0b3e3e
4g-13Eh <shahinamon05@gmail.com>
2024-09-02 12:54:44 +0200

    update sidebar componenet

--- src/app/ui/sidebar/sidebar.component.ts
+++ src/app/ui/sidebar/sidebar.component.ts
@@ -1,18 +1,33 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {categories} from "../../items/item/item.model";
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {CATEGORIES, categories, category} from "../../items/item/item.model";
+import {MatButton} from "@angular/material/button";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatChip, MatChipListbox, MatChipOption, MatChipSelectionChange} from "@angular/material/chips";
+import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+import {ControlValueAccessor} from "@angular/forms";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatMenuTrigger,
+    MatMenu,
+    MatChip,
+    MatIcon,
+    MatChipListbox,
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
- categories!: categories;
- @Output()categorySelected = new EventEmitter();
+  categories: categories = CATEGORIES;
+  @Output() categorySelected = new EventEmitter();
 
- onSelectCategory(category: string){
+ onSelectCategory(category: string, chip: MatChip){
    this.categorySelected.emit(category);
+   chip.highlighted= !chip.highlighted;
  }
 }
