bfdda2b4745dcfa1889360eb9d3e697fc9f1148a
4g-13Eh <shahinamon05@gmail.com>
2024-09-24 11:44:26 +0200

    fixed NullInjectionError for httpClient

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app/app.component';
 import {provideRouter, withComponentInputBinding, withRouterConfig} from "@angular/router";
 import {routes} from "./app/app.routes";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {provideHttpClient} from "@angular/common/http";
 
 
 bootstrapApplication(AppComponent, {
@@ -12,7 +13,7 @@ bootstrapApplication(AppComponent, {
     provideAnimationsAsync(),
     provideRouter(routes, withComponentInputBinding(), withRouterConfig({
       paramsInheritanceStrategy: 'always'
-    })), provideAnimationsAsync(),
+    })), provideAnimationsAsync(), provideHttpClient()
   ]
 })
   .catch((err)=> console.log(err))
